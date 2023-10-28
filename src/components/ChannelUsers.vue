30b062148ac8b49a1a12f58f9056edf45056e021
SVBSH <getenv@protonmail.com>
2023-10-28 13:56:41 +0200

    Add ChannelList component to ChannelList

--- src/components/ChannelUsers.vue
+++ src/components/ChannelUsers.vue
@@ -2,24 +2,21 @@
   <q-separator />
   <q-scroll-area class="col-5">
     <q-list separator v-if="channel != null">
-      <q-item v-for="user in channel.users" :key="user.id">
-        <q-item-section>{{ user.nickname }}</q-item-section>
-        <q-badge v-if="user.state == 'online'" class="bg-green">Online</q-badge>
-        <q-badge v-if="user.state == 'dnd'" class="bg-orange">DND</q-badge>
-        <q-badge v-if="user.state == 'offline'" class="bg-red">Offline</q-badge>
-      </q-item>
+      <ChannelUser v-for="user in channel.users" :user="user" />
     </q-list>
   </q-scroll-area>
 </template>
 
 <script lang="ts">
-import { useChannel } from "src/model/Channel"
-import { defineComponent } from "vue"
+import { useChannel } from "src/model/Channel";
+import { defineComponent } from "vue";
+import ChannelUser from "./ChannelUser.vue";
 
 export default defineComponent({
   setup(props, ctx) {
-    const channel = useChannel()
-    return { channel }
+    const channel = useChannel();
+    return { channel };
   },
-})
+  components: { ChannelUser },
+});
 </script>
