be00d106b18765404791b6306332762781b51407
SVBSH <getenv@protonmail.com>
2023-10-28 13:37:02 +0200

    Create ChannelUser component

---
+++ src/components/ChannelUser.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-item>
+    <q-item-section>{{ user.nickname }}</q-item-section>
+    <q-badge v-if="user.state == 'online'" class="user-state bg-green"
+      >Online</q-badge
+    >
+    <q-badge v-if="user.state == 'dnd'" class="user-state bg-orange"
+      >DND</q-badge
+    >
+    <q-badge v-if="user.state == 'offline'" class="user-state bg-red"
+      >Offline</q-badge
+    >
+  </q-item>
+</template>
+
+<script lang="ts">
+import { User } from "src/model/User";
+import { defineComponent, toRef } from "vue";
+export default defineComponent({
+  props: {
+    user: { type: User, required: true },
+  },
+  setup(props, ctx) {
+    console.log(props.user.nickname);
+
+    return { user: toRef(props, "user") };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.user-state {
+  width: 3.4rem;
+  padding-inline: 0.8em;
+  padding-block: 0.5em;
+}
+</style>
