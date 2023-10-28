83e6e57604d15eb707e24f7afd3213ddcb928a84
SVBSH <getenv@protonmail.com>
2023-10-28 13:55:55 +0200

    Add ChannelList component to ChannelList

--- src/components/ChannelList.vue
+++ src/components/ChannelList.vue
@@ -1,7 +1,14 @@
 <template>
   <q-scroll-area>
     <q-list separator>
-      <q-item v-for="channel in channelList.channels.values()" :key="channel.id" clickable v-ripple :to="`/channel/${channel.id}`">
+      <q-item
+        v-for="channel in channelList.channels.values()"
+        :key="channel.id"
+        clickable
+        v-ripple
+        :to="`/channel/${channel.id}`"
+        @click="selectChannel(channel.id)"
+      >
         <q-item-section>{{ channel.name }}</q-item-section>
       </q-item>
     </q-list>
@@ -9,13 +16,25 @@
 </template>
 
 <script lang="ts">
-import { useChannelList } from "src/model/ChannelList"
-import { defineComponent } from "vue"
-
+import { useChannelList } from "src/model/ChannelList";
+import { defineComponent } from "vue";
+import { useChannelAdapter } from "src/model/Channel";
+import { useRouter } from "vue-router";
 export default defineComponent({
   setup(props, ctx) {
-    const channelList = useChannelList()
-    return { channelList }
+    const channelList = useChannelList();
+    const channelAdapter = useChannelAdapter();
+    const router = useRouter();
+
+    async function selectChannel(channelId: number) {
+      const reqChannel = await channelList.getChannel(channelId);
+      if (reqChannel != undefined) {
+        channelAdapter.setSelectedChannel(reqChannel);
+        router.push(`/channel/${channelId}`);
+        console.log("sekecting");
+      }
+    }
+    return { channelList, selectChannel };
   },
-})
+});
 </script>
