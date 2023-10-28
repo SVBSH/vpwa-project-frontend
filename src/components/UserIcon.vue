42fd716315e58bb6040cc75e383e96b31e2de435
SVBSH <getenv@protonmail.com>
2023-10-28 14:01:36 +0200

    Add info about user status to main toolbar

--- src/components/UserIcon.vue
+++ src/components/UserIcon.vue
@@ -8,22 +8,61 @@
       </q-list>
     </q-menu>
   </q-btn>
+
+  <q-btn flat :class="user.state" :label="user.state">
+    <q-menu>
+      <q-list>
+        <q-item
+          clickable
+          v-ripple
+          v-for="state in inactiveUserState"
+          :class="state"
+          :key="state"
+          @click="changeUserState(state)"
+        >
+          {{ state.toUpperCase() }}
+        </q-item>
+      </q-list>
+    </q-menu>
+  </q-btn>
 </template>
 
 <script lang="ts">
-import { useUserAdapter } from "src/model/User"
-import { defineComponent } from "vue"
+import { useUserAdapter } from "src/model/User";
+import { defineComponent, computed } from "vue";
+import { UserState, USER_STATE } from "src/model/User";
 
 export default defineComponent({
   setup(props, ctx) {
-    const userAdapter = useUserAdapter()
-    const user = userAdapter.getCurrentUser()
+    const userAdapter = useUserAdapter();
+    const user = userAdapter.getCurrentUser();
 
     function logout() {
-      userAdapter.logout()
+      userAdapter.logout();
     }
 
-    return { user, logout }
+    function changeUserState(state: UserState) {
+      userAdapter.setUserState(state);
+    }
+    const inactiveUserState = computed(() =>
+      USER_STATE.filter((state) => state != user.state)
+    );
+
+    return { user, logout, changeUserState, inactiveUserState };
   },
-})
+});
 </script>
+
+<style scoped>
+.online {
+  color: limegreen;
+}
+
+.offline {
+  color: red;
+}
+
+.dnd {
+  color: orange;
+}
+</style>
