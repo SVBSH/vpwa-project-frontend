b7d555c23374692d8f9271ba99ed7b5ed5109a56
SVBSH <getenv@protonmail.com>
2023-10-29 17:10:30 +0100

    add quasar component for user messages

--- src/components/ChannelView.vue
+++ src/components/ChannelView.vue
@@ -1,6 +1,34 @@
 <template>
   <q-scroll-area class="root-area q-pa-md" v-if="channel != null">
-    <div class="q-my-sm" v-for="message in channel.messages" :key="message.id">&lt;{{ message.user.nickname }}&gt; {{ message.content }}</div>
+    <div class="q-pa-md">
+      <q-infinite-scroll @load="" reverse>
+        <!-- TODO: wait for API call -->
+        <!-- <template v-slot:loading>
+          <div class="row justify-center q-my-md">
+            <q-spinner :thickness="2" color="primary" name="dots" size="40px" />
+          </div>
+        </template> -->
+
+        <div
+          v-for="(message, index) in channel.messages"
+          :key="index"
+          class="caption q-py-sm"
+        >
+          <!-- <q-badge class="shadow-1">
+            {{ channel.messages.length - index }}
+          </q-badge> -->
+
+          <q-chat-message
+            :key="message.id"
+            :text="[message.content]"
+            text-color="white"
+            :name="message.user.nickname"
+            bg-color="primary"
+            class="q-my-sm"
+          />
+        </div>
+      </q-infinite-scroll>
+    </div>
   </q-scroll-area>
 </template>
 
@@ -15,13 +43,13 @@
 </style>
 
 <script lang="ts">
-import { useChannel } from "src/model/Channel"
-import { defineComponent } from "vue"
+import { useChannel, useChannelAdapter } from "src/model/Channel";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   setup(props, ctx) {
-    const channel = useChannel()
-    return { channel }
+    const channel = useChannel();
+    return { channel };
   },
-})
+});
 </script>
