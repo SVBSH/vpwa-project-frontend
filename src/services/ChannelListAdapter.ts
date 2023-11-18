eb0f533ae55d21088a7fcb2217bdda2ca06b6c3e
SVBSH <getenv@protonmail.com>
2023-11-18 15:37:04 +0100

    add method for getting channel with API

--- src/services/ChannelListAdapter.ts
+++ src/services/ChannelListAdapter.ts
@@ -1,9 +1,11 @@
-import { Channel, ChannelType } from "src/contracts/Channel"
+import { Channel, ChannelData, ChannelType } from "src/contracts/Channel"
 import { Message } from "src/contracts/Message"
 import { User } from "src/contracts/User"
 import { InjectionKey, Ref, inject, onUnmounted, provide, reactive, ref, watch } from "vue"
 import { useChannelAdapter } from "./ChannelAdapter"
 import { CommandError } from "./errors"
+import { api } from "src/boot/axios"
+import axios from "axios"
 const CHANNEL_LIST_KEY = Symbol("channel-list-key") as InjectionKey<ChannelListAdapter>
 
 // FIXME: will be replaced with an API call
@@ -18,7 +20,22 @@ export class ChannelListAdapter {
    * fetches the user list and a list of messages.
    * */
   public async getChannel(id: number) {
-    return this.channels.get(id)
+    const response = await api.get<ChannelData>(`/api/channel/${id}`)
+
+    const partialChannel = this.channels.get(id)
+    if (partialChannel) {
+      partialChannel.users = response.data.users.map(user => {
+        return new User(user)
+      })
+
+      partialChannel.messages = response.data.messages.map(message => {
+        return new Message({
+          ...message,
+          user: response.data.users.find(user => user.id === message.author)
+        })
+      })
+    }
+    return partialChannel
   }
 
   public async addChannel(channelName: string, channelType: ChannelType, user: User) {
@@ -44,32 +61,15 @@ export class ChannelListAdapter {
     return newChannel
   }
 
-  public async joinChannel(channelName: string, channelType: ChannelType, user: User) {
-    //   const requestedChannel = this.getChannelByName(channelName);
-    //   const channelAdapter = useChannelAdapter()
-    //    // Create a new channel
-    //   if (requestedChannel == null) {
-    //     // TODO: try to create a channel
-    //     const newChannel = await this.addChannel(
-    //       channelName,
-    //       channelType,
-    //       user
-    //     );
-    //     if (newChannel == undefined) {
-    //       return '';
-    //     }
-    //     return `Creating channel <strong>${channelName}</strong>`
-    //   } else if (requestedChannel.type != "public") {
-    //     throw new CommandError(`You could not join to requested channel because <strong>${channelName}</strong> is not a public channel.`)
-    //   } else if (channelAdapter.isMember(user.nickname)) {
-    //     throw new CommandError(`You are <strong>${user.nickname}</strong> already a member of channel <strong>${requestedChannel.name}</strong>.`)
-    //   } else if (channelAdapter.isMemberBanned(user.nickname)) {
-    //     throw new CommandError(`<strong>${user.nickname}</strong> is banned from channel <strong>${requestedChannel.name}</strong>.`)
-    //   } else {
-    //     // Add user to channel
-    //     requestedChannel.users.push(user)
-    //     return `Adding user <strong>${user.nickname}</strong> to channel <strong>${channelName}</strong>`
-    //   }
+  public async joinChannel(chName: string, channelType: ChannelType) {
+    try {
+      const response = await api.post("/api/channel/join", { channelName: chName, isPublic: channelType === "public" })
+      return response.data.message
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        throw new CommandError(error.response.data.message)
+      }
+    }
   }
 
   public async quitChannel(channelId: number, user: User) {
@@ -100,45 +100,7 @@ export class ChannelListAdapter {
   constructor() {
     const self = reactive(this)
     provide(CHANNEL_LIST_KEY, self)
-
-    // Default mock data
-    const user1 = new User({ id: 1, nickname: "foo", state: "dnd" })
-    const user2 = new User({ id: 2, nickname: "bar", state: "offline" })
-    const user3 = new User({ id: 3, nickname: "baz", state: "online" })
-    const user4 = new User({ id: 4, nickname: "buz", state: "dnd" })
-    const user5 = new User({ id: 5, nickname: "asd", state: "online" })
-    const user6 = new User({ id: 6, nickname: "123", state: "online" })
-    const user7 = new User({ id: 7, nickname: "kakak", state: "online" })
-    const user8 = new User({ id: 8, nickname: "wasas", state: "online" })
-    const user9 = new User({ id: 9, nickname: "rtrtr", state: "online" })
-
-    this.channels.set(0, new Channel({
-      id: 0,
-      name: "Channel 1",
-      type: "public",
-      admin: user1,
-      users: [user1, user2, user3, user4, user5, user6, user7, user8, user9],
-      restrictedList: new Map([
-        ["bar", ["asd", "baz"]],
-        ["456", ["asd", "baz", "wasas"]]
-      ]),
-      messages: [
-        new Message({ user: user1, content: "Lorem ipsum dolor sit amet" }),
-        new Message({ user: user2, content: "consectetur adipisicing elit" }),
-        new Message({ user: user3, content: "Beatae, ea at cupiditate quisquam voluptatem modi" }),
-        new Message({ user: user1, content: "neque voluptates omnis est ipsam" })
-      ]
-    }))
-    this.channels.set(1, new Channel({
-      id: 1,
-      name: "Channel 2",
-      type: "private",
-      admin: user2,
-      users: [user1],
-      messages: [
-        new Message({ user: user1, content: "Lorem ipsum dolor sit amet" })
-      ]
-    }))
+    /*
     // Mock for user typing
     setInterval(() => {
       const typing = this.channels.get(0)!.usersTyping
@@ -157,7 +119,7 @@ export class ChannelListAdapter {
 
       typing.set(user3, text)
     }, 1000)
-
+    */
     return self
   }
 }
